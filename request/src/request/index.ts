af556e51486a6e5f0853333810007f2bac674659
Wayne Links <wayne.links@gmail.com>
2020-07-23 00:56:12 +0200

    fix(error): fix expression is not callable error

--- request/src/request/index.ts
+++ request/src/request/index.ts
@@ -1,6 +1,6 @@
 import request from 'supertest'
 
-export default (app: any, url: string, data: string | object, statusCode: number) => {
+export const test = (app: any, url: string, data: string | object, statusCode: number) => {
   return request(app)
     .post(url)
     .send(data)
