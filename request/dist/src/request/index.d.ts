5fcd368965c39213e735b28442fb71a7b81de01b
Wayne Links <wayne.links@gmail.com>
2020-07-23 00:39:24 +0200

    fix(scripts): update pub script

--- request/dist/src/request/index.d.ts
+++ request/dist/src/request/index.d.ts
@@ -1,7 +1,3 @@
 import request from 'supertest';
-<<<<<<< HEAD
-declare const _default: (app: any, url: string, data: any, statusCode: number) => request.Test;
-=======
 declare const _default: (app: any, url: string, data: string | object, statusCode: number) => request.Test;
->>>>>>> request
 export default _default;
--- request/dist/src/request/index.js
+++ request/dist/src/request/index.js
@@ -4,16 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const supertest_1 = __importDefault(require("supertest"));
-<<<<<<< HEAD
-exports.default = (app, url, data, statusCode) => (supertest_1.default(app)
-    .post(`${process.env.BASE_API}/${url}`)
-    .send(data)
-    .expect(statusCode));
-=======
 exports.default = (app, url, data, statusCode) => {
     return supertest_1.default(app)
         .post(url)
         .send(data)
         .expect(statusCode);
 };
->>>>>>> request
