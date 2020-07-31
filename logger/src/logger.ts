29d11d7fc107e0f81910e6705518e7c798c09a7b
Wayne Links <wayne.links@gmail.com>
2020-07-31 11:57:05 +0200

    some updates

--- logger/src/logger.ts
+++ logger/src/logger.ts
@@ -1,28 +1,42 @@
 import { createLogger, format, transports } from 'winston'
+import DailyRotateFile from 'winston-daily-rotate-file'
+
+import fs from 'fs'
+import path from 'path'
 
 const env = process.env.NODE_ENV || 'development'
 
+let dir = './logs'
+if (!dir) dir = path.resolve('logs')
+
+if (!fs.existsSync(dir)) fs.mkdirSync(dir)
+
+const options = {
+  file: {
+    level: env === 'development' ? 'debug' : 'warn',
+    filename: dir + '/%DATE%.log',
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: true,
+    timestamp: true,
+    handleExceptions: true,
+    humanReadableUnhandledException: true,
+    prettyPrint: true,
+    json: true,
+    maxSize: '20m',
+    colorize: true,
+    maxFiles: '14d',
+  },
+}
 const logger = createLogger({
   level: env === 'development' ? 'debug' : 'info',
   format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
-    }),
     format.json(),
+    format.prettyPrint(),
     format.errors({ stack: true }),
     format.splat(),
   ),
-  defaultMeta: { service: process.env.SERVICE },
-  transports: [
-    new transports.File({
-      filename: 'error.log',
-      level: 'error',
-    }),
-    new transports.File({ filename: 'combined.log' }),
-  ],
-  exceptionHandlers: [
-    new transports.File({ filename: 'uncaughtExceptions.log' }),
-  ],
+  exceptionHandlers: [new DailyRotateFile(options.file)],
+  exitOnError: false
 })
 
 if (process.env.NODE_ENV !== 'production') {
