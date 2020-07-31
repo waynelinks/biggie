4d69e4f72e63e54810795faca45fc569cd8ea3f7
Wayne Links <wayne.links@gmail.com>
2020-07-31 09:54:57 +0200

    refactor: add rotating log files and someother adjustments

--- logger/dist/src/logger.js
+++ logger/dist/src/logger.js
@@ -1,29 +1,41 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
 const winston_1 = require("winston");
+const winston_daily_rotate_file_1 = __importDefault(require("winston-daily-rotate-file"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const env = process.env.NODE_ENV || 'development';
-const logger = winston_1.createLogger({
-    level: env === 'development' ? 'debug' : 'info',
-    format: winston_1.format.combine(winston_1.format.timestamp({
-        format: 'YYYY-MM-DD HH:mm:ss',
-    }), winston_1.format.json(), winston_1.format.errors({ stack: true }), winston_1.format.splat()),
-    defaultMeta: { service: process.env.SERVICE },
+let dir = './logs';
+if (!dir)
+    dir = path_1.default.resolve('logs');
+if (!fs_1.default.existsSync(dir))
+    fs_1.default.mkdirSync(dir);
+const options = {
+    file: {
+        level: env === 'development' ? 'debug' : 'warn',
+        filename: dir + '/%DATE%.log',
+        datePattern: 'YYYY-MM-DD',
+        zippedArchive: true,
+        timestamp: true,
+        handleExceptions: true,
+        humanReadableUnhandledException: true,
+        prettyPrint: true,
+        json: true,
+        maxSize: '20m',
+        colorize: true,
+        maxFiles: '14d',
+    },
+};
+exports.default = winston_1.createLogger({
     transports: [
-        new winston_1.transports.File({
-            filename: 'error.log',
-            level: 'error',
+        new winston_1.transports.Console({
+            level: env === 'development' ? 'debug' : 'warn',
+            format: winston_1.format.combine(winston_1.format.errors({ stack: true }), winston_1.format.prettyPrint()),
         }),
-        new winston_1.transports.File({ filename: 'combined.log' }),
     ],
-    exceptionHandlers: [
-        new winston_1.transports.File({ filename: 'uncaughtExceptions.log' }),
-    ],
+    exceptionHandlers: [new winston_daily_rotate_file_1.default(options.file)],
+    exitOnError: false,
 });
-exports.logger = logger;
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston_1.transports.Console({
-        level: 'info',
-        format: winston_1.format.combine(winston_1.format.colorize(), winston_1.format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)),
-    }));
-}
