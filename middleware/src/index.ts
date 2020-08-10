a1bba637fbb81775435d4e49bb6ebbb92e9e2dc3
Wayne Links <wayne.links@gmail.com>
2020-08-10 23:09:56 +0200

    Updates

---
+++ middleware/src/index.ts
@@ -0,0 +1,5 @@
+export * from './middlewares/commonMiddleware'
+export * from './middlewares/currentUser'
+export * from './middlewares/errorHandling'
+export * from './middlewares/requireAuth'
+export * from './middlewares/notFound'
